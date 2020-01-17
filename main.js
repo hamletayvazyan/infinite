a5d76a69b78f32e0e3cf72bcd7ca79d161c2b200
Hamlet <hamlet94@live.ru>
2020-01-17 18:47:55 +0400

    added arrows function and fixed some style bugs

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     var timer = 3000;
+    var positionNum = 4;
     let a = setTimeout(tick, timer);
     setInterval(() => {
         // console.log($('#asd').position().top);
@@ -13,4 +14,30 @@ $(document).ready(function () {
         // console.log('asd', timer);
         $('#asd').addClass('animate-barrier');
     }
+
+    $(document).keyup(function (e) {
+        switch (e.which) {
+            case 38: // up
+                console.log(e);
+                break;
+            case 40: // down
+                console.log(e);
+                break;
+            case 37: // left
+                positionNum = positionNum == 26 ? 4 :
+                    positionNum == 50 ? 26 :
+                    positionNum == 70 ? 50 : 4;
+                $('#car').css('left', positionNum+'%');
+                break;
+            case 39: // right
+                positionNum = positionNum == 4 ? 26 :
+                    positionNum == 26 ? 50 :
+                    positionNum == 50 ? 70 : 70;
+                $('#car').css('left', positionNum+'%');
+                break;
+            default:
+                return // top
+        }
+        e.preventDefault();
+    });
 });
